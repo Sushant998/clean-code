d0c3049692429cebd51882dda5ce9380e449ccba
Sushant998 <92773923+Sushant998@users.noreply.github.com>
2023-06-28 12:22:12 +0530

    [refactor-0000]-added ts file for shopping cart

---
+++ shopping-cart.ts
@@ -0,0 +1,31 @@
+export class ShoppingCartComponent implements OnInit {
+
+  private items: Array<Item> = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addItem(item: Item) {
+    this.items.push(item);
+  }
+
+  removeItem(item: Item) {
+    this.items = this.items.filter(i => i !== item);
+  }
+
+  calculateTotal() {
+    return this.items.reduce((acc, item) => acc + item.price, 0);
+  }
+
+  displayItems() {
+    console.log(this.items);
+  }
+
+}
+
+interface Item {
+  name: string;
+  price: number;
+}
